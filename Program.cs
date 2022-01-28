5bf9a48c12a040dce444876f0452a3406ee83684
Miguel-Bit-Debug <migueldossantos011@gmail.com>
2022-01-27 23:50:23 -0300

    add changes

--- Program.cs
+++ Program.cs
@@ -21,6 +21,7 @@ namespace DemoShop
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://*:" + Environment.GetEnvironmentVariable("PORT"));
                 });
     }
 }
